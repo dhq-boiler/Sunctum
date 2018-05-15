30ae398239e3a49916300acc6a8351db92ad017e
dhq_boiler <dhq_boiler@live.jp>
2018-05-15 19:12:24 +0900

    Fix number incrementing

--- Sunctum/ViewModels/ContentDocumentViewModel.cs
+++ Sunctum/ViewModels/ContentDocumentViewModel.cs
@@ -5,16 +5,17 @@ namespace Sunctum.ViewModels
     {
         private static int s_internalNumber = 0;
         private string _title;
+        private int _number;
 
         public ContentDocumentViewModel(string title)
         {
             _title = title;
-            s_internalNumber++;
+            _number = s_internalNumber++;
         }
 
         public override string Title => _title;
 
-        public override string ContentId => $"content-{s_internalNumber}";
+        public override string ContentId => $"content-{_number}";
 
         public override bool CanClose => true;
     }
