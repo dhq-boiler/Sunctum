2cc66ed1318e91586a1f8252251193a89bb8b691
dhq_boiler <dhq_boiler@live.jp>
2020-11-14 01:59:16 +0900

    Remove unused code.

--- Sunctum.Infrastructure/Data/Yaml/YamlFile.cs
+++ Sunctum.Infrastructure/Data/Yaml/YamlFile.cs
@@ -4,7 +4,6 @@ using System;
 using System.IO;
 using System.Text;
 using YamlDotNet.Serialization;
-using YamlDotNet.Serialization.NamingConventions;
 
 namespace Sunctum.Infrastructure.Data.Yaml
 {
