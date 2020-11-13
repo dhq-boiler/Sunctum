e6641cf60e3196a39bc7c3065d9d0f248efde2e9
dhq_boiler <dhq_boiler@live.jp>
2020-11-13 23:44:32 +0900

    Set the activation of  the StartEncryption button.

--- Sunctum.UI/Dialogs/EncryptionStartingDialog.xaml.cs
+++ Sunctum.UI/Dialogs/EncryptionStartingDialog.xaml.cs
@@ -21,16 +21,34 @@ namespace Sunctum.UI.Dialogs
     /// </summary>
     public partial class EncryptionStartingDialog : Window
     {
-        public string Password { get { return this.password.Password; } }
+        public string Password { get; set; }
 
         public EncryptionStartingDialog()
         {
             InitializeComponent();
+            this.StartEncryption.IsEnabled = false;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
+        }
+
+        private void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            string temp = "|NOMATCH|";
+            List<PasswordBox> pb = new List<PasswordBox>(2) { };
+            List<string> s = new List<string> { password.Password, passwordConfirm.Password };
+            if (s[0] == s[1] && !string.IsNullOrWhiteSpace(s[0]))
+            {
+                temp = s[0];
+                this.StartEncryption.IsEnabled = true;
+            }
+            else
+            {
+                this.StartEncryption.IsEnabled = false;
+            }
+            this.Password = temp;
         }
     }
 }
