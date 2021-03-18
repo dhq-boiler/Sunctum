8979c12c8d4048204244fa651ceed73f1961f356
dhq_boiler <dhq_boiler@live.jp>
2021-03-18 21:40:28 +0900

    modernize UI(Window)

--- Sunctum/Views/MainWindow.xaml.cs
+++ Sunctum/Views/MainWindow.xaml.cs
@@ -44,5 +44,45 @@ namespace Sunctum.Views
         {
             MainWindowVM.Terminate();
         }
+
+        private void CommandBinding_CanExecute_Close(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void CommandBinding_CanExecute_Maximize(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_Maximize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        private void CommandBinding_CanExecute_Minimize(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+        }
+
+        private void CommandBinding_CanExecute_Restore(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_Restore(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.RestoreWindow(this);
+        }
     }
 }
