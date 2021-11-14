a7bcc9cab7b07ad570a483b8b36bd397b2ed7bf4
dhq_boiler <dhq_boiler@live.jp>
2021-11-14 15:46:42 +0900

    organize usings

--- Sunctum.Domain/Logic/Async/CurrentProcessProgress.cs
+++ Sunctum.Domain/Logic/Async/CurrentProcessProgress.cs
@@ -2,11 +2,7 @@
 using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using static Sunctum.Domain.Logic.Async.CurrentProcessProgress;
 
 namespace Sunctum.Domain.Logic.Async
