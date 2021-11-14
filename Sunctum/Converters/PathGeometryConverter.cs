c09707a947a931caee18fac0182bd304f0a816de
dhq_boiler <dhq_boiler@live.jp>
2021-11-14 15:01:37 +0900

    change item progress pie appearance

---
+++ Sunctum/Converters/PathGeometryConverter.cs
@@ -0,0 +1,89 @@
+﻿using Sunctum.Domain.Logic.Async;
+using Sunctum.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Sunctum.Converters
+{
+    internal class PathGeometryConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var cpp = (CurrentProcessProgress)value;
+            var Rate = 270 + 360 * Math.Min(1.0, cpp.Rate.Value);
+            if (Rate > 360)
+                Rate -= 360;
+            return PieGeometry(new Point(62.5, 75), 50, 270, Rate, SweepDirection.Clockwise);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        //完成形、回転方向を指定できるように
+        /// <summary>
+        /// 扇(pie)型のPathGeometryを作成
+        /// </summary>
+        /// <param name="center">中心座標</param>
+        /// <param name="distance">中心点からの距離</param>
+        /// <param name="startDegrees">開始角度、0以上360未満で指定</param>
+        /// <param name="stopDegrees">終了角度、0以上360未満で指定</param>
+        /// <param name="direction">回転方向、Clockwiseが時計回り</param>
+        /// <returns></returns>
+        private PathGeometry PieGeometry(Point center, double distance, double startDegrees, double stopDegrees, SweepDirection direction)
+        {
+            Point start = MakePoint(startDegrees, center, distance);//始点座標
+            Point stop = MakePoint(stopDegrees, center, distance);//終点座標
+            //開始角度から終了角度までが180度を超えているかの判定
+            //超えていたらArcSegmentのIsLargeArcをtrue、なければfalseで作成
+            double diffDegrees = (direction == SweepDirection.Clockwise) ? stopDegrees - startDegrees : startDegrees - stopDegrees;
+            if (diffDegrees < 0) { diffDegrees += 360.0; }
+            bool isLarge = (diffDegrees > 180) ? true : false;
+            var arc = new ArcSegment(stop, new Size(distance, distance), 0, isLarge, direction, true);
+
+            //PathFigure作成
+            //ArcSegmentとその両端と中心点をつなぐ直線LineSegment
+            var fig = new PathFigure();
+            fig.StartPoint = start;//始点座標
+            fig.Segments.Add(arc);//ArcSegment追加
+            fig.Segments.Add(new LineSegment(center, true));//円弧の終点から中心への直線
+            fig.Segments.Add(new LineSegment(start, true));//中心から円弧の始点への直線
+            fig.IsClosed = true;//Pathを閉じる、必須
+
+            //PathGeometryを作成してPathFigureを追加して完成
+            var pg = new PathGeometry();
+            pg.Figures.Add(fig);
+            return pg;
+        }
+
+        /// <summary>
+        /// 距離と角度からその座標を返す
+        /// </summary>
+        /// <param name="degrees">360以上は359.99になる</param>
+        /// <param name="center">中心点</param>
+        /// <param name="distance">中心点からの距離</param>
+        /// <returns></returns>
+        private Point MakePoint(double degrees, Point center, double distance)
+        {
+            if (degrees >= 360) { degrees = 359.99; }
+            var rad = Radian(degrees);
+            var cos = Math.Cos(rad);
+            var sin = Math.Sin(rad);
+            var x = center.X + cos * distance;
+            var y = center.Y + sin * distance;
+            return new Point(x, y);
+        }
+        private double Radian(double degree)
+        {
+            return Math.PI / 180.0 * degree;
+        }
+    }
+}
