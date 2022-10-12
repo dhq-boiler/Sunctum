0a7405018f2c9aafb86ecab3a5739919666c75fe
dhq_boiler <dhq_boiler@live.jp>
2022-10-13 00:04:32 +0900

    remove unused code

--- Sunctum.Domain/Data/Dao/BookDao.cs
+++ Sunctum.Domain/Data/Dao/BookDao.cs
@@ -12,7 +12,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace Sunctum.Domain.Data.Dao
